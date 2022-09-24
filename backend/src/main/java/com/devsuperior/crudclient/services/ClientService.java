b36143e6a2476da264d785ef8a52905531aa7f19
Rodrigo Emanuel <drigosantos@gmail.com>
2022-09-24 09:50:28 -0300

    End point delete

--- backend/src/main/java/com/devsuperior/crudclient/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/crudclient/services/ClientService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.crudclient.dto.ClientDTO;
 import com.devsuperior.crudclient.entities.Client;
+import com.devsuperior.crudclient.exceptions.DatabaseException;
 import com.devsuperior.crudclient.exceptions.ResourceNotFoundException;
 import com.devsuperior.crudclient.repositories.ClientRepository;
 
@@ -52,6 +55,16 @@ public class ClientService {
 			return new ClientDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrit violation");
 		}
 	}
 	
